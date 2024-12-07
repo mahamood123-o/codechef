474ebf493e0d0d9fe4423fbf2dc723fe9ae6c629
Mahamood <shaikmisba305@gmail.com>
2024-12-07 14:38:31 +0530

    Add files via upload

---
+++ aicom.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class aicom
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n<=60)
+		System.out.println("YES");
+		else
+		System.out.println("NO");
+		// your code goes here
+
+	}
+}
