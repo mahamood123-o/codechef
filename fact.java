f9ff001156f5c384a7406b8f11904da36aa358f5
Mahamood <shaikmisba305@gmail.com>
2024-12-08 15:27:12 +0530

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,23 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            System.out.println(factorial(n));
+        }
+        
+        sc.close();
+    }
+    
+    public static BigInteger factorial(int n) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 1; i <= n; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        return result;
+    }
+}
