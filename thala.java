fbc448fc0f2d5a082f1469f44edab543a4960a78
Mahamood <shaikmisba305@gmail.com>
2024-12-08 12:16:21 +0530

    Add files via upload

---
+++ thala.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class thala {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        if (N == 7) {
+            System.out.println("THALA");
+        } else {
+            System.out.println("SADGE");
+        }
+    }
+}
