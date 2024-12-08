27b3d2dd87d527dd2c721ecec11b9316be581efc
Mahamood <shaikmisba305@gmail.com>
2024-12-08 16:06:31 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        if (X > 35) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
