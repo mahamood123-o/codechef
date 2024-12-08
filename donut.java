c12760e74a79c0820ba9ed5b9b0983ce115ed7c3
Mahamood <shaikmisba305@gmail.com>
2024-12-08 14:28:32 +0530

    Add files via upload

---
+++ donut.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+        System.out.println(X * Y);
+        sc.close();
+    }
+}
