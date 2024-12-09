3de8c950f10508e15a14dd471c471769f276d52d
Mahamood <shaikmisba305@gmail.com>
2024-12-09 09:55:49 +0530

    Add files via upload

---
+++ gold.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class gold {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+        
+        if (X > Y) {
+            System.out.println(A);
+        } else {
+            System.out.println(B);
+        }
+    }
+}
