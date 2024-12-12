ade368c67e1a9af1680d38c0b0f381859a0deef7
Mahamood <shaikmisba305@gmail.com>
2024-12-12 13:32:23 +0530

    Add files via upload

---
+++ email.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class ContestReminders {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int U = sc.nextInt();
+        
+        System.out.println(N - U);
+    }
+}
