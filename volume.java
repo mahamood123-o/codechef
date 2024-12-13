f4c68dabdccada62fb980af655436736c05aea11
Mahamood <shaikmisba305@gmail.com>
2024-12-13 12:30:33 +0530

    Add files via upload

---
+++ volume.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        
+        for (int i = 0; i < T; i++) {
+            int X = sc.nextInt();
+            int Y = sc.nextInt();
+            System.out.println(Math.abs(X - Y));
+        }
+    }
+}
