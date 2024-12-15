1e0f3c21b84777cb4e33ec7b0f69d9796488cc84
Mahamood <shaikmisba305@gmail.com>
2024-12-15 10:05:54 +0530

    Add files via upload

---
+++ miami.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        
+        for (int i = 0; i < T; i++) {
+            int X = sc.nextInt();
+            int Y = sc.nextInt();
+            if (Y <= 1.07 * X) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
